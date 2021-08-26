4ba7403e9c616c8a84c84591d12967b634478c80
19825856221 <1030550074@qq.com>
2021-08-26 20:34:38 +0800

    123

--- TMS/Controllers/ApproveController.cs
+++ TMS/Controllers/ApproveController.cs
@@ -114,6 +114,89 @@ namespace TMS.Controllers
         }
         #endregion
 
+        #region//承运审核
+        /// <summary>
+        /// 货主显示
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
+        [HttpPost, Route("ConsignorShow")]
+        public IActionResult CarriageShow()
+        {
+            try
+            {
+                int num;
+                List<CarriagecontractModel> list = dal.ConsignorShow();
+                if (list != null)
+                {
+                    return Ok(list);
+                }
+                else
+                {
+                    num = 2;
+                    return Ok(num);
+                }
+            }
+            catch (Exception)
+            {
+
+                return Ok("数据异常");
+            }
+        }
+        /// <summary>
+        /// 货主审核成功
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost, Route("ConsignorEditTG")]
+        public IActionResult CarriageEditTG(int id)
+        {
+            try
+            {
+                int list = dal.ConsignorEditTG(id);
+                if (list != 0)
+                {
+                    return Ok("审核通过");
+                }
+                else
+                {
+                    return Ok("修改失败");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //return Ok("数据异常");
+            }
+        }
+
+        /// <summary>
+        /// 货主审核失败
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost, Route("ConsignorEditJJ")]
+        public IActionResult CarriageEditJJ(int id)
+        {
+            try
+            {
+                int list = dal.ConsignorEditJJ(id);
+                if (list != 0)
+                {
+                    return Ok("拒绝审核");
+                }
+                else
+                {
+                    return Ok("修改失败");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //return Ok("数据异常");
+            }
+        }
+        #endregion
 
 
     }
